3ec94018c42730d33049bbfc6fd0cec19ecb8bd0
okphong582-png <okphong582@gmail.com>
2026-07-28 13:10:15 +0700

    Update InitBase offset to 0xA988FDC

--- esp/Core/GameLogic.mm
+++ esp/Core/GameLogic.mm
@@ -3,7 +3,7 @@
 #pragma mark - Function Game
 
 uint64_t getMatchGame(uint64_t Moudule_Base) {
-    uint64_t GameFacade_TypeInfo = ReadAddr<uint64_t>(Moudule_Base + 0xA986B7C);
+    uint64_t GameFacade_TypeInfo = ReadAddr<uint64_t>(Moudule_Base + 0xA988FDC);
     uint64_t GameFacade_Static = ReadAddr<uint64_t>(GameFacade_TypeInfo + 0xB8);
     return ReadAddr<uint64_t>(GameFacade_Static + 0x0);
 }
